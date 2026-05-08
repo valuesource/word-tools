553f405abd6ba37dbdb92d8b22aef23443285fea
valuesource <willie8765@gmail.com>
2026-05-07 20:43:06 -0500

    Add Microsoft Clarity

--- app/layout.js
+++ app/layout.js
@@ -31,6 +31,15 @@ export default function RootLayout({ children }) {
     gtag('config', 'G-Y2S27B0Z0Y');
   `}
 </Script>
+<Script id="microsoft-clarity" strategy="afterInteractive">
+  {`
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "wnnu98cxqk");
+  `}
+</Script>
         {children}
 
         <footer
